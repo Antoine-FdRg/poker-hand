8ce0165ab5de701a379237720c8d0aef23944fd4
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2023-10-13 17:04:22 +0200

    add enum #18

---
+++ src/main/java/com/seinksansdoozebank/fr/controller/Rank.java
@@ -0,0 +1,34 @@
+package com.seinksansdoozebank.fr.controller;
+
+public enum Rank {
+    AS("A", 14),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    VALET("V", 11),
+    QUEEN ("D",12),
+    KING("R",13);
+
+    private String symbol;
+    private int value;
+    Rank(String symbol, int value) {
+        this.symbol=symbol;
+        this.value=value;
+    }
+
+
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
