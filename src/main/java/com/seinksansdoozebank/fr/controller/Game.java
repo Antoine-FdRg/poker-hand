4456f4c7976abda96a21dc560ce65cb796590878
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-10-18 19:48:42 +0200

    :sparkles: Completed 2 TODOs on Cli.java Number of cards are now checked Size of Hand is also checked

--- src/main/java/com/seinksansdoozebank/fr/controller/Game.java
+++ src/main/java/com/seinksansdoozebank/fr/controller/Game.java
@@ -9,8 +9,8 @@ public class Game {
 
     public void run(){
         Cli view = new Cli();
-        Hand hand1 = new Hand(view.initializeHand(1));
-        Hand hand2 = new Hand(view.initializeHand(2));
+        Hand hand1 = new Hand(view.initializeHand(1,1));
+        Hand hand2 = new Hand(view.initializeHand(2,1));
         Referee referee = new Referee();
         Victory winner = referee.compareHands(hand1, hand2);
         if(winner==null){
