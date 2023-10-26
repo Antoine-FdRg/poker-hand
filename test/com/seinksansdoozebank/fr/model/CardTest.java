4275d7bda42435b60315052b0f779002955dc709
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2023-10-26 20:58:46 +0200

    :white_check_mark: add other tests

--- test/com/seinksansdoozebank/fr/model/CardTest.java
+++ test/com/seinksansdoozebank/fr/model/CardTest.java
@@ -6,21 +6,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
-    private Card card1;
-    private Card card2;
-    private Card card3;
+    private Card cardEight;
+    private Card cardThree;
+    private Card cardThreeBis;
+    private Card cardKing;
+    private Card cardQueen;
+    private Card cardJack;
+    private Card cardAce;
+    private Card cardAceBis;
 
     @BeforeEach
     void setUp() {
-        this.card1 = new Card(Rank.EIGHT);
-        this.card2 = new Card(Rank.THREE);
-        this.card3 = new Card(Rank.THREE);
+        this.cardEight = new Card(Rank.EIGHT);
+        this.cardThree = new Card(Rank.THREE);
+        this.cardThreeBis = new Card(Rank.THREE);
+        this.cardKing = new Card(Rank.KING);
+        this.cardQueen = new Card(Rank.QUEEN);
+        this.cardJack = new Card(Rank.JACK);
+        this.cardAce = new Card(Rank.ACE);
+        this.cardAceBis = new Card(Rank.ACE);
     }
 
     @Test
     void compareTo() {
-        assertTrue(card1.compareTo(card2)>0);
-        assertTrue(card2.compareTo(card1)<0);
-        assertEquals(0,card3.compareTo(card2));
+        assertTrue(cardEight.compareTo(cardThree)>0);
+        assertTrue(cardThree.compareTo(cardEight)<0);
+        assertEquals(0, cardThreeBis.compareTo(cardThree));
+        assertTrue(cardKing.compareTo(cardAce)<0);
+        assertTrue(cardKing.compareTo(cardQueen)>0);
+        assertTrue(cardJack.compareTo(cardAce)<0);
+        assertTrue(cardJack.compareTo(cardEight)>0);
+        assertTrue(cardAce.compareTo(cardEight)>0);
+        assertTrue(cardThree.compareTo(cardQueen)<0);
+        assertEquals(0, cardAce.compareTo(cardAceBis));
     }
 }
