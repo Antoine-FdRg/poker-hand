9754f241a9444e02f16633952ea03e7c2cd24c10
Clément LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-11-10 15:08:56 +0100

    :ambulance: Fix of duplicatesManagement
    Was keeping the deck even if the input was false, now a temporary deck is used before checking all the input is correct

--- test/com/seinksansdoozebank/fr/controller/GameTest.java
+++ test/com/seinksansdoozebank/fr/controller/GameTest.java
@@ -16,6 +16,7 @@ class GameTest {
     void setUp() {
         int nbCards = 3;
         this.game = new Game(2, nbCards);
+        Game.resetDeck();
     }
 
     @Test
@@ -37,6 +38,21 @@ class GameTest {
         cards.remove(2);
         Game.resetDeck();
         cards.add("2Ca");
+        assertTrue(game.checkInput(cards));
+    }
+
+    /**
+     * CheckInput tests for duplicates Management
+     */
+    @Test
+    void checkInputDuplicates(){
+        List<String> cards = new ArrayList<>();
+        cards.add("2Ca");
+        cards.add("2Ca");
+        cards.add("2Co");
+        assertFalse(game.checkInput(cards));
+        cards.remove(1);
+        cards.add("2Pi");
         assertTrue(game.checkInput(cards));
     }
 }
